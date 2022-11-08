f11085dc936df179f6941e66ad7bbb2ed161979c
SabbirHowlader <sabbirahmedreza56@gmail.com>
2022-11-08 21:36:39 +0600

    added header

--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -3,30 +3,36 @@ import logo from '../../Asset/logo/logo.jpg'
 
 const Footer = () => {
     return (
-        <footer className="footer p-32 bg-black text-white">
-            <div>
-                <img src={logo} alt=""  style={{heigh:110, width:110}} className="rounded"/>
-                <p>Home made Food Delivery Service.<br />Providing reliable food since 2009.</p>
+        <footer className=' bg-black' >
+            <div className="footer p-32 text-white">
+                <div>
+                    <img src={logo} alt="" style={{ heigh: 110, width: 110 }} className="rounded" />
+                    <p>Home made Food Delivery Service.<br />Providing reliable food since 2009.</p>
+                </div>
+                <div>
+                    <span className="footer-title">Services</span>
+                    <a href='/' className="link link-hover">Burger</a>
+                    <a href='/' className="link link-hover">Pizza</a>
+                    <a href='/' className="link link-hover">Chicken Fry</a>
+                    <a href='/' className="link link-hover">Pasta</a>
+                </div>
+                <div>
+                    <span className="footer-title">Own Company</span>
+                    <a href='/' className="link link-hover">About us</a>
+                    <a href='/' className="link link-hover">Contact</a>
+                    <a href='/' className="link link-hover">Service</a>
+                    <a href='/' className="link link-hover">Order</a>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <a href='/' className="link link-hover">Terms of use</a>
+                    <a href='/' className="link link-hover">Privacy policy</a>
+                    <a href='/' className="link link-hover">Copiright policy</a>
+                </div>
             </div>
-            <div>
-                <span className="footer-title">Services</span>
-                <a href='/' className="link link-hover">Burger</a>
-                <a href='/' className="link link-hover">Pizza</a>
-                <a href='/' className="link link-hover">Chicken Fry</a>
-                <a href='/' className="link link-hover">Pasta</a>
-            </div>
-            <div>
-                <span className="footer-title">Own Company</span>
-                <a href='/' className="link link-hover">About us</a>
-                <a href='/' className="link link-hover">Contact</a>
-                <a href='/' className="link link-hover">Service</a>
-                <a href='/' className="link link-hover">Order</a>
-            </div>
-            <div>
-                <span className="footer-title">Legal</span>
-                <a href='/' className="link link-hover">Terms of use</a>
-                <a href='/' className="link link-hover">Privacy policy</a>
-                <a href='/' className="link link-hover">Copiright policy</a>
+            <br />
+            <div className=' text-white pb-6'>
+                <p>Copyright © 2022 - All right reserved by <span className='text-orange-600'>FOOD HOUSE</span></p>
             </div>
         </footer>
     );
--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../Asset/logo/logo.jpg'
 
 const Header = () => {
-    return (
-        <div>
-            <h2>header</h2>
-        </div>
-    );
-};
 
+    const menubar =
+        <>
+            <li className='font-semibold'><Link to='/'>Home</Link></li>
+            <li className='font-semibold'><Link to='/'>Service</Link></li>
+        </>
+    return (
+        <div className="navbar bg-base-300">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                       {menubar}
+                    </ul>
+                </div>
+                <img src={logo} alt="" style={{height:60, width:60}} className="rounded-full"/>
+                <a  href='/' className="btn btn-warning btn-ghost normal-case text-xl ">FOOD HOUSE</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal p-0">
+                    {menubar}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a href='/' className="btn ">Food Search</a>
+            </div>
+        </div>
+    )
+}
 export default Header;
