647d04f33de38a6445e59b23e71405b9e0e83c4f
SabbirHowlader <sabbirahmedreza56@gmail.com>
2022-11-09 12:21:17 +0600

    added signup page

--- src/Page/SignUp/SignUp.js
+++ src/Page/SignUp/SignUp.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import img from '../../Asset/signup/signup.jpg'
+import { AuthContext } from '../../Context/AuthContext/AuthProvider';
 
 const SignUp = () => {
+    const {createUser} = useContext(AuthContext);
+    const handleSignUp = event =>{
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        createUser(email, password)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(err => console.error(err));
+    }
+
     return (
-        <div className="hero min-h-screen bg-base-200 my-20">
+        <div className="hero min-h-screen w-5/6 mx-auto bg-base-200 my-20">
             <div className="hero-content gap-5 grid md:grid-cols-2 flex-col lg:flex-row">
                 <div className="text-center lg:text-left">
-                    <img className='w-3/4' src={img} alt="" />
+                    <img className='w-3/4' src={img} style={{height:490}} alt="" />
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <h1 className="text-5xl font-bold text-center">Sign Up</h1>
-                    <form  className="card-body">
+                    <form onSubmit={handleSignUp} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Name</span>
